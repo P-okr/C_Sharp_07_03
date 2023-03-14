5fd69b8be13843e6096334962f5e554d75887e3b
P-okr <112502241+P-okr@users.noreply.github.com>
2023-03-14 22:12:11 +0300

    Lessin_1_Task_1

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// 1 задание пз 
+
+Console.WriteLine("Write a number : ");
+
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num * num);
